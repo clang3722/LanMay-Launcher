002b39c095abd9e54361638393fb136b51c8c981
clang3722 <3165324653@qq.com>
2024-10-12 22:18:38 +0800

    888

---
+++ GetWindowsApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+
+namespace LanMay_Launcher
+{
+    internal class GetWindowsApi
+    {
+        public int GetWindowsAvailableMBytes()
+        {
+            string PhysicalMemory = string.Empty;
+            var managementClass = new ManagementClass("Win32_ComputerSystem");
+            var managementObjectCollection = managementClass.GetInstances();
+            foreach (var managementBaseObject in managementObjectCollection)
+                if (managementBaseObject["TotalPhysicalMemory"] != null) { 
+                    PhysicalMemory = managementBaseObject["TotalPhysicalMemory"].ToString();
+            }
+            
+            long PhysicalMemoryTo = long.Parse(PhysicalMemory) / 1024 / 1024 - 128;
+            return (int)PhysicalMemoryTo;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using StarLight_Core.Authentication;
 using StarLight_Core.Enum;
 using StarLight_Core.Launch;
+using StarLight_Core.Models.Authentication;
 using StarLight_Core.Models.Launch;
 using StarLight_Core.Utilities;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Diagnostics;
+using System;
+using System.IO;
 
 namespace LanMay_Launcher
 {
@@ -37,7 +42,11 @@ namespace LanMay_Launcher
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             var account = new OfflineAuthentication(PlayerName.Text).OfflineAuth();
-
+            // Get windows Memroy to setting Maxmemory
+            var getWinAPI = new GetWindowsApi();
+            int systemMemory = (int)((int)initMemory.Value * getWinAPI.GetWindowsAvailableMBytes() * 0.01);
+           // systemMemory = System.Convert.ToInt32(systemMemory);
+            Console.WriteLine("System total Memory Size: ", getWinAPI.GetWindowsAvailableMBytes() );
             LaunchConfig args = new() // 配置启动参数
             {
                 Account = new()
@@ -50,13 +59,15 @@ namespace LanMay_Launcher
                     Version = GameVersion.Text, // 启动的版本
                     IsVersionIsolation = true, //版本隔离
                 },
+
                 JavaConfig = new()
                 {
                     JavaPath = JavaPath.Text, // Java 路径(绝对路径)
-                    MaxMemory = 4000,
-                    MinMemory = 1000
+                    MaxMemory = (int)systemMemory,
+                    MinMemory = 1024*1
                 }
             };
+         
             var launch = new MinecraftLauncher(args); // 实例化启动器
             var la = await launch.LaunchAsync(ReportProgress); // 启动
             //kh
@@ -78,6 +89,37 @@ namespace LanMay_Launcher
         private void ReportProgress(ProgressReport report)
         {
             Progress.Text = report.Description + " " + report.Percentage + "%";
+        }
+
+        private void showMemory_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            
+            var getWinAPI = new GetWindowsApi();
+            int systemMemory = (int)((int)initMemory.Value * getWinAPI.GetWindowsAvailableMBytes() * 0.01);
+            showMemory.Content = "使用运行内存百分比：" + (int)initMemory.Value + "%" + " " + systemMemory + "MB";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+                // 获取当前应用程序的目录
+                string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                // 构建要打开的文件夹路径
+                string folderPath = Path.Combine(currentDirectory, ".minecraft\\versions\\"); // 替换为您想要打开的子文件夹名称
+
+                // 使用Process.Start打开文件夹
+                try
+                {
+                    ProcessStartInfo OpenFiles =  new ProcessStartInfo("explorer.exe", $"/select,\"{folderPath}");
+                    OpenFiles.UseShellExecute = true;
+                    Process.Start(OpenFiles);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"无法打开文件夹: {ex.Message}");
+                }
+            
         }
     }
 }
