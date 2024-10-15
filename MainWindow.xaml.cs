1d576f438bf4da57cddb879adfe656f25c364f31
clang3722 <3165324653@qq.com>
2024-10-15 22:12:36 +0800

    更新了背景和微软登录（韦测试）（没时间了）

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows;
 using System.Diagnostics;
 using System;
 using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace LanMay_Launcher
 {
@@ -23,6 +25,8 @@ namespace LanMay_Launcher
 
             GerGameVer();
             GetJavas();
+
+            LoginMode.SelectedIndex = 1;
         }
 
         void GerGameVer()
@@ -41,12 +45,38 @@ namespace LanMay_Launcher
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var account = new OfflineAuthentication(PlayerName.Text).OfflineAuth();
+            BaseAccount account;
+            
+            if(LoginMode.SelectedIndex == 0)
+            {
+                var auth = new MicrosoftAuthentication("a5631472 - c813 - 4c35 - 987d - b6e728964cb2");
+                var code = await auth.RetrieveDeviceCodeInfo();
+                Clipboard.Clear();
+                Clipboard.SetText(code.UserCode);
+                MessageBox.Show(code.UserCode);
+                Process.Start(new ProcessStartInfo(code.VerificationUri)
+                {
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+                var token = await auth.GetTokenResponse(code);
+                account = await auth.MicrosoftAuthAsync(token, x =>
+                {
+                    Progress.Text = x;
+                });
+            }
+            else
+            {
+                account = new OfflineAuthentication(PlayerName.Text).OfflineAuth();
+            }
+            
             // Get windows Memroy to setting Maxmemory
             var getWinAPI = new GetWindowsApi();
             int systemMemory = (int)((int)initMemory.Value * getWinAPI.GetWindowsAvailableMBytes() * 0.01);
            // systemMemory = System.Convert.ToInt32(systemMemory);
             Console.WriteLine("System total Memory Size: ", getWinAPI.GetWindowsAvailableMBytes() );
+
+
             LaunchConfig args = new() // 配置启动参数
             {
                 Account = new()
@@ -67,7 +97,6 @@ namespace LanMay_Launcher
                     MinMemory = 1024*1
                 }
             };
-         
             var launch = new MinecraftLauncher(args); // 实例化启动器
             var la = await launch.LaunchAsync(ReportProgress); // 启动
             //kh
@@ -99,6 +128,7 @@ namespace LanMay_Launcher
             showMemory.Content = "使用运行内存百分比：" + (int)initMemory.Value + "%" + " " + systemMemory + "MB";
         }
 
+        //打开版本文件夹
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
                 // 获取当前应用程序的目录
@@ -119,7 +149,67 @@ namespace LanMay_Launcher
                 {
                     Console.WriteLine($"无法打开文件夹: {ex.Message}");
                 }
-            
+        }
+
+        //打开背景文件夹
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // 获取当前应用程序的目录
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // 构建要打开的文件夹路径
+            string folderPath = Path.Combine(currentDirectory, "background\\"); // 替换为您想要打开的子文件夹名称
+
+            // 使用Process.Start打开文件夹
+            try
+            {
+                ProcessStartInfo OpenFiles = new ProcessStartInfo("explorer.exe", $"/select,\"{folderPath}");
+                OpenFiles.UseShellExecute = true;
+                Process.Start(OpenFiles);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"无法打开文件夹: {ex.Message}");
+            }
+        }
+        
+        //创建背景图片文件夹
+        public partial class App : Application
+        {
+            protected override void OnStartup(StartupEventArgs e)
+            {
+                base.OnStartup(e);
+
+                string directoryPath = "background"; // 这里填写您想要创建的文件夹名称
+                CheckAndCreateDirectory(directoryPath);
+            }
+
+            private void CheckAndCreateDirectory(string directoryPath)
+            {
+                string currentDirectory = Environment.CurrentDirectory; // 获取当前应用程序目录
+                string fullPath = Path.Combine(currentDirectory, directoryPath); // 组合成完整路径
+
+                if (!Directory.Exists(fullPath)) // 检查文件夹是否存在
+                {
+                    Directory.CreateDirectory(fullPath); // 如果不存在，则创建文件夹
+                }
+            }
+        }
+
+        //登录
+        private void LoginMode_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(LoginMode.SelectedIndex == 0)
+            {
+                PlayerNameText.Visibility = Visibility.Collapsed;
+                PlayerName.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                PlayerNameText.Visibility = Visibility.Visible;
+                PlayerName.Visibility = Visibility.Visible;
+            }
         }
     }
 }
